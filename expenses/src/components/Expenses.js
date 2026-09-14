7f1b380a0ecb4645773101bf92ca9e6cbdfcdd31
markusvarula-svg <markus.varula@voco.ee>
2026-09-14 08:41:46 +0000

    feat

--- expenses/src/components/Expenses.js
+++ expenses/src/components/Expenses.js
@@ -1,13 +1,15 @@
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import Card from './Card';
 
 const Expenses = (props) => {
+  
   return (
-    <div className='expenses'>
+    <Card className='expenses'>
       {props.data.map((expense, index) => (
         <ExpenseItem key={index} data={expense} />
       ))}
-    </div>
+    </Card>
   );
 };
 
